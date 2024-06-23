5d30ecee621d1393328635e82b691210252e500c
Fabián Morag <fabianmorag0405@gmail.com>
2024-06-23 17:27:30 -0400

    references section added

---
+++ src/contants/references.js
@@ -0,0 +1,8 @@
+export const REFERENCES = [
+  {
+    name: 'Candelaria Ferreira',
+    image: '/candelaria-ferreira.webp',
+    role: 'Experimentation and Personalization | A/B-tests | CRO Specialist',
+    rec: 'Fabián es una parte valiosa de nuestro equipo en D2B.\nSu calidez y buen trato crean siempre un ambiente positivo para todos. Destaco siempre su aporte super valioso a nuestra dinámica de CRO con sus habilidades en cuestiones de desarrollo y su enfoque organizado en cada tarea que emprende.\nSus habilidades técnicas y su actitud colaborativa son reconocidas por todos los miembros del equipo.'
+  }
+]
