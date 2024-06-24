1b183ac45fb981e0f2d559ca12c66e0699534b54
Fabián Morag <fabianmorag0405@gmail.com>
2024-06-24 00:22:56 -0400

    project in constants file & added images to projects

---
+++ src/contants/projects.js
@@ -0,0 +1,38 @@
+export const PROJECTS = [
+  {
+    name: 'D2B - Sitio web oficial',
+    img: '/d2b.webp',
+    description: 'Este es el sitio web de la consultora D2B donde muestra sus servicios, contacto, y su blog. Fue desarrollado utilizando Astro y Tailwind CSS en el frontend para optimizar el rendimiento y el diseño del sitio. En el backend, integra WordPress y Directus para la gestión de contenido principal y personalización avanzada de datos.',
+    siteUrl: 'https://dev.d2b.cl/',
+    srcCodeUrl: 'https://github.com/CRO-D2B/web-d2b-2024',
+    mainBtn: 'Visitar sitio',
+    secBtn: 'Código'
+  },
+  {
+    name: 'E-commerce',
+    img: '/my-ecommerce.webp',
+    description: 'Proyecto demo de un e-commerce ficticio que utiliza Next.js como su framework principal de desarrollo, aprovechando su capacidad para lograr una experiencia de usuario rápida y fluida, apoyado de un desarrollo enfocado en UX. Cuenta con una API desarrollada con GraphQL que ofrece una forma eficiente de consultar y manipular datos a través de un único punto de acceso.',
+    siteUrl: 'https://my-ecommerce-five-pied.vercel.app/',
+    srcCodeUrl: 'https://github.com/FabianMorag/my-ecommerce',
+    mainBtn: 'Ver desarrollo',
+    secBtn: 'Código'
+  },
+  {
+    name: 'Landing Page',
+    img: '/my-gym.webp',
+    description: 'Landing page demo de un gimnasio ficticio desarrollada usando Astro como framework principal y Tailwind CSS para optimizar el rendimiento y el diseño del sitio. Además, se realiza una simulacion de contratación o de contacto.',
+    siteUrl: 'https://my-gym-peach.vercel.app/',
+    srcCodeUrl: 'https://github.com/FabianMorag/my-gym',
+    mainBtn: 'Demo',
+    secBtn: 'Código'
+  },
+  {
+    name: 'Juego de encontrar cartas iguales',
+    img: '/match-pairs.webp',
+    description: 'Un juego simple pero de mucha lógica, donde el objetivo es encontrar todas las cartas iguales en el menor número de intentos posible. Desarrollado con React y Sass para el diseño y la interacción del juego.',
+    siteUrl: 'https://fabianmorag.github.io/match-pairs',
+    srcCodeUrl: 'https://github.com/FabianMorag/match-pairs',
+    mainBtn: 'Demo',
+    secBtn: 'Código'
+  }
+]
