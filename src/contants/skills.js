ba123cab596a9166667c94d5c8ea1f76e0f08322
Fabián Morag <fabianmorag0405@gmail.com>
2024-06-24 19:28:34 -0400

    skills in constant file

---
+++ src/contants/skills.js
@@ -0,0 +1,71 @@
+export const SKILLS =
+  [
+    {
+      name: 'Html',
+      imageSrc: '/software/html.webp'
+    },
+    {
+      name: 'Css',
+      imageSrc: '/software/css.webp'
+    },
+    {
+      name: 'Javascript',
+      imageSrc: '/software/javascript.webp'
+    },
+    {
+      name: 'Typescript',
+      imageSrc: '/software/typescript.webp'
+    },
+    {
+      name: 'Git',
+      imageSrc: '/software/git.webp'
+    },
+    {
+      name: 'GitHub',
+      imageSrc: '/software/github.webp'
+    },
+    {
+      name: 'Nodejs',
+      imageSrc: '/software/nodejs.webp'
+    },
+    {
+      name: 'TailwindCss',
+      imageSrc: '/software/tailwindcss.webp'
+    },
+    {
+      name: 'Sass',
+      imageSrc: '/software/sass.webp'
+    },
+    {
+      name: 'Astro',
+      imageSrc: '/software/astro.webp'
+    },
+    {
+      name: 'Nextjs',
+      imageSrc: '/software/nextjs.webp'
+    },
+    {
+      name: 'Wordpress',
+      imageSrc: '/software/wordpress.webp'
+    },
+    {
+      name: 'Directus',
+      imageSrc: '/software/directus.webp'
+    },
+    {
+      name: 'MySQL',
+      imageSrc: '/software/mysql.webp'
+    },
+    {
+      name: 'PostgreSQL',
+      imageSrc: '/software/postgresql.webp'
+    },
+    {
+      name: 'GCP',
+      imageSrc: '/software/gcp.webp'
+    },
+    {
+      name: 'Nginx',
+      imageSrc: '/software/nginx.webp'
+    }
+  ]
