3bc46b453a23a848c734ab3dea4edd56d6428156
Fabián Morag <fabianmorag0405@gmail.com>
2024-06-24 18:48:25 -0400

    experience in constant file

---
+++ src/contants/experience.js
@@ -0,0 +1,23 @@
+export const EXPERIENCE = [
+  {
+    role: 'Analista Frontend',
+    company: 'D2B',
+    description: 'Trabajo en el equipo de Optimización de la Tasa de Conversión (CRO) de la empresa, donde me encargo de la planificación y ejecución de experimentos web basados en análisis de datos. Lidero reuniones con clientes y contribuyo en la creación de HTML para correos electrónicos. Además, he participado activamente en el desarrollo y modificaciones de diversos sitios web internos y de clientes. Utilizo herramientas especializadas como ABTasty y Google Analytics 4 para pruebas y análisis de datos.',
+    startDate: { dateTime: '2022-07', textTime: 'Jul 2022' },
+    endDate: { textTime: 'Actualidad' }
+  },
+  {
+    role: 'Soporte Técnico y Mantenimiento',
+    company: 'Universidad Austral de Chile',
+    description: 'Brindé soporte técnico y mantenimiento al proyecto TrAC, involucrándome en la revisión de la configuración del servidor, la corrección de vulnerabilidades y la generación periodica de respaldos de bases de datos. Además, desarrollé documentación detallada de los procesos llevados a cabo durante este periodo. Mi trabajo principal se centró en la gestión del servidor y la configuración de la aplicación en Docker.',
+    startDate: { dateTime: '2021-05', textTime: 'May 2021' },
+    endDate: { dateTime: '2021-11', textTime: 'Nov 2021' }
+  },
+  {
+    role: 'Ingeniero de Software',
+    company: 'Puentes Digitales (Práctica profesional)',
+    description: 'Como practicante en la empresa consultora, participé en la creación de nuevas interfaces y funcionalidades para el proyecto TrAC. El desarrollo se llevó a cabo bajo metodologías ágiles, utilizando historias de usuario recopiladas por los ingenieros responsables. Las principales herramientas empleadas incluyeron NextJS, bases de datos SQL y sistemas de gestión de tareas.',
+    startDate: { dateTime: '2021-01', textTime: 'Ene 2021' },
+    endDate: { dateTime: '2021-03', textTime: 'Marzo 2021' }
+  }
+]
