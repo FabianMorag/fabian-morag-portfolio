6d0b88c7ef09ab3f752c1025d48a2e2dd7e773cb
Fabián Morag <fabianmorag0405@gmail.com>
2024-06-25 17:04:14 -0400

    new reference

--- src/contants/references.js
+++ src/contants/references.js
@@ -4,5 +4,11 @@ export const REFERENCES = [
     image: '/candelaria-ferreira.webp',
     role: 'Experimentation and Personalization | A/B-tests | CRO Specialist',
     rec: 'Fabián es una parte valiosa de nuestro equipo en D2B.\nSu calidez y buen trato crean siempre un ambiente positivo para todos. Destaco siempre su aporte super valioso a nuestra dinámica de CRO con sus habilidades en cuestiones de desarrollo y su enfoque organizado en cada tarea que emprende.\nSus habilidades técnicas y su actitud colaborativa son reconocidas por todos los miembros del equipo.'
+  },
+  {
+    name: 'Barbara Mujica Molteni',
+    image: '/barbara-mujica.webp',
+    role: 'UX/UI Designer',
+    rec: 'Tuve el placer de trabajar con Fabián en D2B. Como Analista Front End, destaca por su excelente conocimiento técnico y su colaboración. Fabián siempre convirtió mis diseños en interfaces atractivas y funcionales, colaborando de manera efectiva para garantizar la mejor calidad en cada proyecto. Su capacidad para adaptarse a nuevas tecnologías y su dedicación lo hacen un valioso miembro de cualquier equipo.'
   }
 ]
