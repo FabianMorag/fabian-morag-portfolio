b6cb9b60677f56e05118264feebd290fbba18e58
Fabián Morag <fabianmorag0405@gmail.com>
2024-06-24 21:24:36 -0400

    basic seo: priority added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,15 @@ import sitemap from '@astrojs/sitemap'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://fabianmorag.com',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap({
+    customPages: ['https://fabianmorag.com/CV-Fabian-Morag.pdf'],
+    priority: 1.00,
+    serialize (item) {
+      if (/CV-Fabian-Morag.pdf/.test(item.url)) {
+        item.priority = 0.80
+      }
+      return item
+    }
+  }
+  )]
 })
