59a211cc58c581b3c37f69143e4804a1db66b043
Fabián Morag <fabianmorag0405@gmail.com>
2024-07-01 14:10:45 -0400

    new description for experience block

--- src/contants/experience.js
+++ src/contants/experience.js
@@ -2,21 +2,37 @@ export const EXPERIENCE = [
   {
     role: 'Analista Frontend',
     company: 'D2B',
-    description: 'Trabajo en el equipo de Optimización de la Tasa de Conversión (CRO) de la empresa, donde me encargo de la planificación y ejecución de experimentos web basados en análisis de datos. Lidero reuniones con clientes y contribuyo en la creación de HTML para correos electrónicos. Además, he participado activamente en el desarrollo y modificaciones de diversos sitios web internos y de clientes. Utilizo herramientas especializadas como ABTasty y Google Analytics 4 para pruebas y análisis de datos.',
+    description: [
+      'Liderar reuniones, planificación y ejecución de experimentos web para VTR Claro.',
+      'Contribución en la creación de HTML para correos de aerolínea SKY.',
+      'Levantamiento de infraestructura (GCP) y desarrollo frontend/backend para sitio web de D2B',
+      'Participación activa en el desarrollo y modificaciones de diversos sitios web internos y de clientes.',
+      'Uso de herramientas especializadas en AB test y análisis, como ABTasty y Google Analytics 4.'
+    ],
     startDate: { dateTime: '2022-07', textTime: 'Jul 2022' },
     endDate: { textTime: 'Actualidad' }
   },
   {
     role: 'Soporte Técnico y Mantenimiento',
     company: 'Universidad Austral de Chile',
-    description: 'Brindé soporte técnico y mantenimiento al proyecto TrAC, involucrándome en la revisión de la configuración del servidor, la corrección de vulnerabilidades y la generación periodica de respaldos de bases de datos. Además, desarrollé documentación detallada de los procesos llevados a cabo durante este periodo. Mi trabajo principal se centró en la gestión del servidor y la configuración de la aplicación en Docker.',
+    description: [
+      'Soporte técnico y mantenimiento al proyecto TrAC.',
+      'Configuración del servidor, la corrección de vulnerabilidades (Docker y Nginx).',
+      'Respaldos periodicos de bases de datos.',
+      'Creación de documentación de los procesos llevados a cabo durante este periodo.'
+    ],
     startDate: { dateTime: '2021-05', textTime: 'May 2021' },
     endDate: { dateTime: '2021-11', textTime: 'Nov 2021' }
   },
   {
     role: 'Ingeniero de Software',
     company: 'Puentes Digitales (Práctica profesional)',
-    description: 'Como practicante en la empresa consultora, participé en la creación de nuevas interfaces y funcionalidades para el proyecto TrAC. El desarrollo se llevó a cabo bajo metodologías ágiles, utilizando historias de usuario recopiladas por los ingenieros responsables. Las principales herramientas empleadas incluyeron NextJS, bases de datos SQL y sistemas de gestión de tareas.',
+    description: [
+      'Desarrollo de nuevas interfaces y funcionalidades para el proyecto TrAC.',
+      'Todo el desarrollo frontend y backend con NextJS.',
+      'Manejo de base de datos SQL.',
+      'Entorno de trabajo con metodologías ágiles.'
+    ],
     startDate: { dateTime: '2021-01', textTime: 'Ene 2021' },
     endDate: { dateTime: '2021-03', textTime: 'Marzo 2021' }
   }
