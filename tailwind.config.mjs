4923ecd54280cf6db5dddc94982a65696bb2309c
Fabián Morag <fabianmorag0405@gmail.com>
2024-09-04 22:24:43 -0400

    mobile carrousel for references

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('tailwindcss-animated')
-	],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('tailwindcss-animated'),
+    function ({ addComponents }) {
+      addComponents({
+        '.hidden-scrollbar': {
+          '-ms-overflow-style': 'none',
+          'scrollbar-width': 'none',
+          '&::-webkit-scrollbar': {
+            display: 'none'
+          }
+        }
+      })
+    }
+  ]
 }
