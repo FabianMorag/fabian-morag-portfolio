9aaa590827e6b0ded20cbb662256a0845fae8c90
Fabián Morag <fabianmorag0405@gmail.com>
2025-01-19 20:56:10 -0300

    susana reference

--- src/contants/references.js
+++ src/contants/references.js
@@ -1,5 +1,11 @@
 export const REFERENCES = [
   {
+    name: 'Susana Dallal de Silva',
+    image: '/susana-dallal.webp',
+    role: 'UX Writer | CRO Analyst',
+    rec: 'Fabián es una persona noble, paciente y con una ética profesional que destaca desde el primer momento. Tuve el privilegio de trabajar junto a él, y desde entonces demostró una actitud ejemplar.\nSiempre dispuesto a ayudar, a compartir sus conocimientos y a afrontar nuevos retos con compromiso, convirtiéndolo en un profesional altamente valioso, especialmente en áreas tecnológicas.\nEstoy segura de que cualquier equipo que cuente con él no solo ganará un gran profesional, sino también una excelente persona. ¡Súper recomendado!'
+  },
+  {
     name: 'Candelaria Ferreira',
     image: '/candelaria-ferreira.webp',
     role: 'Experimentation and Personalization | A/B-tests | CRO Specialist',
